5131440dfc1402e1f5d1df21f28bee7a96554871
geraldarzy <geraldarzola@gmail.com>
2021-02-23 09:22:58 -0500

    commit tree e5addecb9153090c1d88e1c98fb7829ebf2dbe44

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum:250}
+    validates :summary, length: { maximum:250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+    validate :clickbait_title?
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top [0-9]*/i,
+        /Guess/i
+    ]
+    
+    def clickbait_title?
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+          errors.add(:title, "must be clickbait")
+        end
+    end
+    
 end
